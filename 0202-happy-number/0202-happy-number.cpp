f5cd6e933c36a4863dee2f3f6cd5a3a3106ce1d6
Anubhavgupta321 <143418991+Anubhavgupta321@users.noreply.github.com>
2026-07-17 01:00:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (38.98%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int getnext(int n){
+        int ans=0;
+        while(n>0){
+            int digit=n%10;
+            ans+=digit*digit;
+            n=n/10;
+        }
+        return ans;
+    }
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while(n!=1 && !seen.count(n)){
+            seen.insert(n);
+            n=getnext(n);
+        }
+        return n==1;
+    }
+};
